4a803c71c5ad95fe77f7b839d3cbdbedd7073eae
JamahaW <153020210+JamahaW@users.noreply.github.com>
2024-12-16 23:20:56 +0300

    upd

--- Embedded/src/main.cpp
+++ Embedded/src/main.cpp
@@ -3,6 +3,14 @@
 #include "servomotor/MockServo.hpp"
 
 
+servomotor::Config<int32_t, int8_t> config = {
+    .speed_regulator_settings {1.0, 0.1, 0.5, 127},
+    .position_regulator_settings {1.0, 1.0, 1.0, 1000000},
+    .max_position_error = 5
+};
+
+servomotor::MockServoMotor m(config);
+
 void setup() {
 }
 
