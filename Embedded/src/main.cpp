8e9bd5f46567057b9404c9ac0a0dfe2f01cbf0bb
JamahaW <153020210+JamahaW@users.noreply.github.com>
2024-12-18 23:12:49 +0300

    little-endian confirm

--- Embedded/src/main.cpp
+++ Embedded/src/main.cpp
@@ -30,10 +30,21 @@ void MemIO_print(const MemIO &mem) {
     Serial.println((size_t) mem.cursor);
 }
 
+
 void setup() {
     Serial.begin(115200);
 
-    serializer.write((int32_t) 0x12345678);
+    struct Foo {
+        uint32_t a;
+        uint16_t b;
+        uint8_t c;
+    } foo{
+        .a = 0xA1A2A3A4,
+        .b = 0xB1B2,
+        .c = 0x69
+    };
+
+    serializer.write(foo);
 
     MemIO_print(stream.output);
 }
