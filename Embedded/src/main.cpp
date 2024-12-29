7f6527353d278e44ffc069f43776d55133104f45
JamahaW <153020210+JamahaW@users.noreply.github.com>
2024-12-29 14:39:44 +0300

    test motor encoder add GA25Encoder.hpp fir Tools.hpp

--- Embedded/src/main.cpp
+++ Embedded/src/main.cpp
@@ -1,22 +1,73 @@
-#include "serialcmd/StreamBinarySerializer.hpp"
+#include "servomotor/drivers/L293Driver.hpp"
+#include "servomotor/encoders/GA25Encoder.hpp"
+#include "serialcmd/Protocol.hpp"
+#include "serialcmd/Types.hpp"
 #include <Arduino.h>
 
-serialcmd::StreamBinarySerializer serializer(Serial);
+
+using servomotor::drivers::L293Driver;
+using servomotor::Direction;
+using servomotor::encoders::GA25Encoder;
+
+const auto test_driver = L293Driver(6, 7, Direction::reverse);
+
+static void test_encoder_interrupt();
+
+const auto test_encoder = GA25Encoder(3, 9, test_encoder_interrupt, Direction::reverse);
+
+void test_encoder_interrupt() { test_encoder.onEncoderMoved(); }
+
+namespace cmd {
+
+    using serialcmd::StreamSerializer;
+
+    enum Result : serialcmd::u8 {
+        ok = 0x00,
+        fail = 0x01,
+    };
+
+    void setMotorSpeed(StreamSerializer &serializer) {
+        serialcmd::i16 pwm_dir;
+
+        serializer.read(pwm_dir);
+        test_driver.setPower(pwm_dir);
+
+        serializer.write(Result::ok);
+    }
+
+    void getEncoderPosition(StreamSerializer &serializer) {
+        serialcmd::i32 position = test_encoder.getPosition();
+        serializer.write(Result::ok);
+        serializer.write(position);
+    }
+
+    void getEncoderSpeed(StreamSerializer &serializer) {
+        serialcmd::i8 speed = test_encoder.getSpeed();
+        serializer.write(Result::ok);
+        serializer.write(speed);
+    }
+
+    typedef void(*Cmd)(StreamSerializer &);
+
+    Cmd commands[]{
+        setMotorSpeed,
+        getEncoderPosition,
+        getEncoderSpeed,
+    };
+}
+
+serialcmd::Protocol<serialcmd::u8, serialcmd::u8> protocol(
+    cmd::commands,
+    3,
+    Serial
+);
 
 void setup() {
     Serial.begin(115200);
 
-    struct Foo {
-        uint32_t a;
-        uint16_t b;
-        uint8_t c;
-    } foo{
-        .a = 0xA1A2A3A4,
-        .b = 0xB1B2,
-        .c = 0x69
-    };
-
-    serializer.write(foo);
+    protocol.begin(0x01);
 }
 
-void loop() {}
+void loop() {
+    protocol.pull();
+}
